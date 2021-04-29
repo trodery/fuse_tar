3f89569a945b0c21f6f6ba04f59262c1a64203af
Terry Rodery <terry@coalitioninc.com>
2021-04-29 16:04:33 -0500

    Add test for root inode attributes

--- tests/test_fuse.py
+++ tests/test_fuse.py
@@ -4,14 +4,16 @@ import logging
 import multiprocessing
 import os
 import pathlib
+import stat
 import sys
 import threading
+import time
 
 import llfuse
 import pytest
 from fuse_tar import TarFS
 
-from util import cleanup, fuse_test_marker, umount, wait_for_mount, sha1sum
+from util import cleanup, fuse_test_marker, sha1sum, umount, wait_for_mount
 
 pytestmark = fuse_test_marker()
 
@@ -65,6 +67,44 @@ def testfs(request, tmpdir):
       umount(mount_process, mnt_dir)
 
 
+@pytest.fixture
+def start_fs(request, tmpdir):
+  """Does the multiprocessing handling so that the filesystem
+  can be mounted and tested against simultaneously"""
+
+  tar_filename = request.param['tar_filename']
+  directory_path = str(pathlib.Path(__file__).parent.absolute())
+  tar_fullpath = f"{directory_path}/{tar_filename}"
+  tmp_dir = tmpdir
+
+  # We can't use forkserver because we have to make sure
+  # that the server inherits the per-test stdout/stderr file
+  # descriptors.
+  if hasattr(multiprocessing, 'get_context'):
+    mp = multiprocessing.get_context('fork')
+  else:
+    # Older versions only support *fork* anyway
+    mp = multiprocessing
+  if threading.active_count() != 1:
+    raise RuntimeError("Multi-threaded test running is not supported")
+
+  mnt_dir = str(tmp_dir)
+  with mp.Manager() as mgr:
+    cross_process = mgr.Namespace()
+    mount_process = mp.Process(target=run_fs,
+                               args=(mnt_dir, cross_process, tar_fullpath))
+
+    mount_process.start()
+    try:
+      wait_for_mount(mount_process, mnt_dir)
+      yield (mnt_dir, cross_process, request.param)
+    except:
+      cleanup(mnt_dir)
+      raise
+    else:
+      umount(mount_process, mnt_dir)
+
+
 @pytest.mark.parametrize('testfs', [0, 1, 2, 3],
                          ids=[
                              'tarfile1.tar-513.txt', 'tarfile1.tar-10241.txt',
@@ -97,12 +137,47 @@ def test_correct_read_file_contents(testfs):
     f"Expected file to has sha1 of {expected_sha1} but is actually {observed_sha1} for file '{member_filename}'"
 
 
+@pytest.mark.parametrize('start_fs', [{
+    'tar_filename': 'tarfile1.tar',
+}],
+                         ids=['tarfile1.tar'],
+                         indirect=True)
+def test_root_inode_attributes(start_fs):
+  """Test that the root inode attributes are properly set"""
+
+  (mnt_dir, _, params) = start_fs
+  root_stat = os.stat(mnt_dir)
+
+  assert root_stat.st_mode == (stat.S_IFDIR | 0o755), \
+    f"Root inode has wrong st_mode {root_stat.st_mode}"
+
+  assert root_stat.st_size == 0, \
+    f"Root inode size is expected to be 0 but is {root_stat.st_size}"
+
+  # Set the timestamp to one hour in the past and checked if
+  # the inode's timestamp is greater than this value
+  timestamp = int((time.time() - 3600) * 1e9)
+
+  assert root_stat.st_atime_ns > timestamp, \
+    f"Expected root inode st_atime_ns ({root_stat.st_atime_ns})> timestamp ({timestamp})"
+
+  assert root_stat.st_ctime_ns > timestamp, \
+    f"Expected root inode st_atime_ns ({root_stat.st_ctime_ns})> timestamp ({timestamp})"
+
+  assert root_stat.st_mtime_ns > timestamp, \
+    f"Expected root inode st_atime_ns ({root_stat.st_mtime_ns})> timestamp ({timestamp})"
+
+  assert root_stat.st_ino == llfuse.ROOT_INODE, \
+    f"Expected root inode number to be {llfuse.ROOT_INODE} but is {root_stat.st_ino}"
+
+
 class Fs(TarFS):
   """Wrapper around TarFS so we can capture the multiprocessing process"""
   def __init__(self, tarname: str, cross_process):
     super(Fs, self).__init__(tarname)
     self.status = cross_process
     self.status.lookup_called = False
+    # self.status.root_inode
 
 
 def run_fs(mountpoint: str, cross_process, path_to_tar: str):
